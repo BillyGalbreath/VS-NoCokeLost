adebd731aaeb9ef9390e6d8738b6b064ab4ef45a
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-10-29 11:09:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# NoCokeLost
+
+All coal turns to coke in coke furnaces. No more losing layers.
---
+++ src/NoCokeLostMod.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Reflection;
+using HarmonyLib;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.GameContent;
+
+namespace NoCokeLost;
+
+[HarmonyPatch]
+public class NoCokeLostMod : ModSystem {
+    private Harmony? harmony;
+
+    public override bool ShouldLoad(EnumAppSide side) {
+        return side.IsServer();
+    }
+
+    public override void StartServerSide(ICoreServerAPI capi) {
+        harmony = new Harmony(Mod.Info.ModID);
+        harmony.PatchAll(Assembly.GetExecutingAssembly());
+    }
+
+    public override void Dispose() {
+        harmony?.UnpatchAll(Mod.Info.ModID);
+    }
+
+    [HarmonyTranspiler]
+    [HarmonyPatch(typeof(BlockEntityCoalPile), "onBurningTickServer")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public static IEnumerable<CodeInstruction> OnBurningTickServer(IEnumerable<CodeInstruction> instructions) {
+        var codes = new List<CodeInstruction>(instructions);
+
+        for (int i = 0; i < codes.Count; i++) {
+            CodeInstruction code = codes[i];
+            if (!code.operand?.ToString()?.Equals("coke") ?? true) {
+                continue;
+            }
+
+            codes.RemoveRange(i + 8, 5);
+            break;
+        }
+
+        return codes.AsEnumerable();
+    }
+}
